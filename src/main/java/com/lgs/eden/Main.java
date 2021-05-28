de7faa24a490acad193866d5f055648e7d9a2055
Raphiki <raphael.soranzo@ensiie.fr>
2021-05-28 16:30:18 +0200

    created some packages, moved files..

--- src/main/java/com/lgs/eden/Main.java
+++ src/main/java/com/lgs/eden/Main.java
@@ -1,3 +1,5 @@
+package com.lgs.eden;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
---
+++ src/main/java/com/lgs/eden/application/Controller.java
@@ -0,0 +1,4 @@
+package com.lgs.eden.application;
+
+public class Controller {
+}
