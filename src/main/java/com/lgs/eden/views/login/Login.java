d7bcd800fff40ae99efc1506d7db6568cf1fea9f
Raphiki <raphael.soranzo@ensiie.fr>
2021-05-28 21:04:52 +0200

    Almost finished task "after_login_view" just waiting for utils and api to have some files/methods

--- src/main/java/com/lgs/eden/views/login/Login.java
+++ src/main/java/com/lgs/eden/views/login/Login.java
@@ -2,6 +2,7 @@ package com.lgs.eden.views.login;
 
 
 import com.lgs.eden.api.Constants;
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
@@ -9,6 +10,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class Login {
 
@@ -34,7 +41,7 @@ public class Login {
 
 
     /**
-     * Action ton submit the login data to the API
+     * Action to submit the login data to the API
      * @param ignore never used
      */
     @FXML
@@ -43,6 +50,8 @@ public class Login {
         int username = login.getText().length();
         int wordpass = password.getText().length();
 
+
+        // TODO: add popups related to logins errors
         // testing username compatibility with the API
         if(username < Constants.LOGIN_MIN_LENGTH || username > Constants.TEXT_MAX_LENGTH) {
             System.out.println("wrong username");
@@ -54,8 +63,34 @@ public class Login {
             test = false;
         }
 
-        // TODO: Add a real submit method
-        if(test) System.out.println("submitted");
+        // TODO: Add a real submit method & real stock data methods
+        if(test) {
+            if(rememberMe.isSelected())
+                System.out.println("Stocking username: " + login.getText());
+            else
+                System.out.println("Removing username :" + login.getText());
+
+            System.out.println("submitted");
+        }
+    }
+
+
+    /**
+     * Action to redirect the user to the link to recover his password
+     * @param ignore nothing to do with it
+     */
+    @FXML
+    public void onForgotPassword(MouseEvent ignore) { // rename @ignore if you use it
+
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            try {
+                // TODO: check language
+                Desktop.getDesktop().browse(new URI("https://lgs-games.com/en/password_forgot"));
+            } catch (URISyntaxException | IOException ignoreMeTooBlink) {
+                // TODO: add popup related to these exceptions
+                System.out.println("exception has occured");
+            }
+        }
     }
 
 
