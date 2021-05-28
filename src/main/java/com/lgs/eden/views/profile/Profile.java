f0390c0dfc85379f3cfc5734ef5db179b1790b4a
Raphiki <raphael.soranzo@ensiie.fr>
2021-05-28 23:38:20 +0200

    added the attributes

---
+++ src/main/java/com/lgs/eden/views/profile/Profile.java
@@ -0,0 +1,17 @@
+package com.lgs.eden.views.profile;
+
+import java.util.Date;
+
+public class Profile {
+
+    private int usernameID;
+    private int friendsNumber;
+    private int reputation;
+    private short status;
+
+    private String biography;
+
+    private Date lastSeen;
+    private Date memberSinceDate;
+
+}
