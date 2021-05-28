40acef17e8a904f115eae6a8fc3a7da767194def
Raphiki <raphael.soranzo@ensiie.fr>
2021-05-28 22:33:32 +0200

    added the method return icon image

--- src/main/java/com/lgs/eden/utils/Utility.java
+++ src/main/java/com/lgs/eden/utils/Utility.java
@@ -1,4 +1,14 @@
 package com.lgs.eden.utils;
 
+import com.lgs.eden.Main;
+import javafx.scene.image.Image;
+
+import java.util.Objects;
+
 public class Utility {
+
+    public Image appIcon() {
+        return new Image(Objects.requireNonNull(Main.class.getResourceAsStream("/icon64.png")));
+    }
+
 }
