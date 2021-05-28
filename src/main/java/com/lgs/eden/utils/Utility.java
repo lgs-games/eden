d16e2ad6bd62e47577160218ad8bcfee9c2def46
Raphiki <raphael.soranzo@ensiie.fr>
2021-05-28 23:13:43 +0200

    added loadView method

--- src/main/java/com/lgs/eden/utils/Utility.java
+++ src/main/java/com/lgs/eden/utils/Utility.java
@@ -1,7 +1,10 @@
 package com.lgs.eden.utils;
 
 import com.lgs.eden.Main;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
+
+import java.net.URL;
 import java.util.Objects;
 
 
@@ -11,8 +14,22 @@ public class Utility {
      * Is used to call the app icon
      * @return an image containing the default app icon
      */
-    public Image appIcon() {
-        return new Image(Objects.requireNonNull(Main.class.getResourceAsStream("/icon64.png")));
+    public Image appIcon() {return new Image(Objects.requireNonNull(Main.class.getResourceAsStream("/icon64.png")));}
+
+
+    /**
+     * A method made to load the views with only the path
+     * @param path path to the view needed
+     * @return an FXMLLoader containing the view
+     * @throws IllegalStateException if the path is not reachable
+     */
+    public FXMLLoader loadView(String path) throws IllegalStateException {
+
+        URL resource = Main.class.getResource(path);
+        if(resource == null)
+            throw new IllegalStateException();
+
+        return new FXMLLoader(resource);
     }
 
 }
