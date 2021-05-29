7f326466bce9e20d32149e8feb1f90f87b7b1b06
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-05-29 12:48:48 +0200

    fix inverted size

--- src/main/java/com/lgs/eden/utils/Config.java
+++ src/main/java/com/lgs/eden/utils/Config.java
@@ -16,8 +16,8 @@ public class Config {
 
     private static final String VERSION = "1.0";
     public static final String APP_NAME = "Eden";
-    public static final int SCREEN_WIDTH = 500;
-    public static final int SCREEN_HEIGHT = 700;
+    public static final int SCREEN_WIDTH = 700;
+    public static final int SCREEN_HEIGHT = 500;
 
     // ------------------------------ CLASS VARIABLES ----------------------------- \\
 
