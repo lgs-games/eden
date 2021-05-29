bb6197d3ee390742923e58a847cfa95a15c3f9c1
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-05-29 18:44:52 +0200

    adding recent games cards

--- src/main/java/com/lgs/eden/views/profile/Profile.java
+++ src/main/java/com/lgs/eden/views/profile/Profile.java
@@ -1,8 +1,10 @@
 package com.lgs.eden.views.profile;
 
 import com.lgs.eden.utils.Utility;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.layout.GridPane;
 
 import java.util.Date;
 
@@ -18,7 +20,10 @@ public class Profile {
      */
     public static Parent getScreen() {
         FXMLLoader loader = Utility.loadView("/fxml/profile.fxml");
-        return Utility.loadViewPane(loader);
+        Parent parent = Utility.loadViewPane(loader);
+        Profile controller = loader.getController();
+        controller.init();
+        return parent;
     }
 
     // ------------------------------ INSTANCE ----------------------------- \\
@@ -32,5 +37,21 @@ public class Profile {
 
     private Date lastSeen;
     private Date memberSinceDate;
+
+    @FXML
+    private GridPane recentGames;
+
+    public Profile() {
+    }
+
+    private void init() {
+        // load some cards
+        Parent g1 = Utility.loadViewPane("/fxml/profile/card.fxml");
+        Parent g2 = Utility.loadViewPane("/fxml/profile/card.fxml");
+        Parent g3 = Utility.loadViewPane("/fxml/profile/card.fxml");
+        this.recentGames.add(g1, 0,0);
+        this.recentGames.add(g2, 1,0);
+        this.recentGames.add(g3, 2,0);
+    }
 
 }
