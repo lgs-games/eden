036449f348c29a5e44a8ea7affe62b4b80e92bcc
Raphiki <raphael.soranzo@ensiie.fr>
2021-05-29 14:02:15 +0200

    changed apiVersion from attribute to just method

--- src/main/java/com/lgs/eden/api/Api.java
+++ src/main/java/com/lgs/eden/api/Api.java
@@ -3,12 +3,6 @@ package com.lgs.eden.api;
 
 public class Api {
 
-    private static String apiVersion;
-
-    public Api(){
-        apiVersion = "1.0";
-    }
-
-    public static String getApiVersion() { return apiVersion; }
+    public static String getApiVersion() { return "1.0"; }
 
 }
