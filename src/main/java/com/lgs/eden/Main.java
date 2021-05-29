518b5772070052ed366c3d7ed5abac2a48313f6c
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-05-29 21:48:01 +0200

    undo main changes

--- src/main/java/com/lgs/eden/Main.java
+++ src/main/java/com/lgs/eden/Main.java
@@ -4,7 +4,6 @@ import com.goxr3plus.fxborderlessscene.borderless.BorderlessScene;
 import com.lgs.eden.application.WindowController;
 import com.lgs.eden.utils.Config;
 import com.lgs.eden.views.login.Login;
-import com.lgs.eden.views.profile.Profile;
 import javafx.application.Application;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -33,7 +32,7 @@ public class Main extends Application {
         BorderlessScene scene = new BorderlessScene(primaryStage, StageStyle.UNDECORATED, root,
                 0, 0);
         // init frame
-        WindowController.init(scene, 1100, 700);
+        WindowController.init(scene, Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
         // then set scene
         primaryStage.setScene(scene);
         // window title
@@ -43,6 +42,6 @@ public class Main extends Application {
         // and show
         primaryStage.show();
         // show login screen
-        WindowController.setScreen(Profile.getScreen());
+        WindowController.setScreen(Login.getScreen());
     }
 }
