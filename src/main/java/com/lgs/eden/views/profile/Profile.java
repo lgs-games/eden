c83be13d15f684f849be85907d846bce8de8df94
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-05-29 19:44:56 +0200

    test loading some recent games

--- src/main/java/com/lgs/eden/views/profile/Profile.java
+++ src/main/java/com/lgs/eden/views/profile/Profile.java
@@ -1,12 +1,18 @@
 package com.lgs.eden.views.profile;
 
+import com.lgs.eden.api.Api;
+import com.lgs.eden.api.wrapper.RecentGameData;
+import com.lgs.eden.utils.Config;
+import com.lgs.eden.utils.Translate;
 import com.lgs.eden.utils.Utility;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
 import java.util.Date;
+import java.util.ResourceBundle;
 
 /**
  * Controller for profile.fxml
@@ -28,6 +34,7 @@ public class Profile {
 
     // ------------------------------ INSTANCE ----------------------------- \\
 
+    private final RecentGameData[] recentGamesData;
     private int usernameID;
     private int friendsNumber;
     private int reputation;
@@ -42,16 +49,34 @@ public class Profile {
     private GridPane recentGames;
 
     public Profile() {
+        // todo: that's a fake of API data
+        this.recentGamesData = new RecentGameData[]{
+                new RecentGameData(Utility.loadImage("/games/prim-icon.png"), "Prim", 0, RecentGameData.PLAYING),
+                new RecentGameData(Utility.loadImage("/games/enigma-icon.png"), "Enigma", 1020, 30)
+        };
     }
 
     private void init() {
-        // load some cards
-        Parent g1 = Utility.loadViewPane("/fxml/profile/card.fxml");
-        Parent g2 = Utility.loadViewPane("/fxml/profile/card.fxml");
-        Parent g3 = Utility.loadViewPane("/fxml/profile/card.fxml");
-        this.recentGames.add(g1, 0,0);
-        this.recentGames.add(g2, 1,0);
-        this.recentGames.add(g3, 2,0);
+        int gamePlayedCount = 2; // todo: fake
+
+        // show the last 3 games
+        if (recentGamesData.length > 0){
+            for (int column = 0; column < 3 && column < gamePlayedCount; column++) {
+                // create
+                FXMLLoader loader = Utility.loadView("/fxml/profile/card.fxml");
+                Parent card = Utility.loadViewPane(loader);
+                // fill
+                ((RecentGameCard)loader.getController()).init(recentGamesData[column]);
+                // add
+                this.recentGames.add(card, column,0);
+            }
+        } else {
+            // no recent games played
+            Label l = new Label(Translate.getTranslation("game-recent-none"));
+            l.getStyleClass().add("profile-game-label");
+            this.recentGames.add(l, 0,0, 3, 1);
+        }
+
     }
 
 }
---
+++ src/main/java/com/lgs/eden/views/profile/RecentGameCard.java
@@ -0,0 +1,37 @@
+package com.lgs.eden.views.profile;
+
+import com.lgs.eden.api.wrapper.RecentGameData;
+import com.lgs.eden.utils.Translate;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+/**
+ * Controller for card.fxml
+ */
+public class RecentGameCard {
+
+    public Label gameName; // game name: Enigma
+    public Label lastPlayed; // ex: 5 days ago
+    public Label lastPlayedLabel; // "last played" label
+    public Label lastPlayedUnit; // "last played" label
+    public Label time_played; // ex: 5 min., 10 hours., ...
+    public ImageView game_icon; // game icon
+
+    public void init(RecentGameData data) {
+        this.gameName.setText(data.gameName);
+        this.game_icon.setImage(data.gameIcon);
+        if (data.isPlaying()) {
+            // hide last played
+            this.lastPlayedLabel.setText(Translate.getTranslation("in_game"));
+            this.lastPlayedLabel.getStyleClass().add("profile-game-playing");
+            this.lastPlayed.setVisible(false);
+            this.lastPlayedUnit.setVisible(false);
+        } else { // show last played xxx days
+            this.lastPlayedLabel.getStyleClass().remove("profile-game-playing");
+            this.lastPlayed.setVisible(true);
+            this.lastPlayedUnit.setVisible(true);
+            this.lastPlayed.setText(data.lastPlayed+"");
+        }
+        this.time_played.setText(data.timePlayed+"");
+    }
+}
