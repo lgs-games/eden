96769c08915b8a98547698fe25991b2d93681aff
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-05-31 15:38:34 +0200

    better module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,21 @@
+/**
+ * This is our game launcher "eden"
+ * module info.
+ */
 module eden {
+    // need this libraries
     requires com.goxr3plus.fxborderlessscene;
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.base;
     requires java.desktop;
 
+    // our main module
     exports com.lgs.eden;
-    exports com.lgs.eden.application;
 
+    // for fxml
     opens com.lgs.eden.application;
     opens com.lgs.eden.utils.helper;
-
     opens com.lgs.eden.views.login;
     opens com.lgs.eden.views.settings;
     opens com.lgs.eden.views.register;
