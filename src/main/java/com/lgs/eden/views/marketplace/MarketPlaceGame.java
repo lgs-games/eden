dda3a0cb5dd3a8c660f844ca26031cc8d649393b
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-06-04 00:05:40 +0200

    marketplace game controller

--- src/main/java/com/lgs/eden/views/marketplace/MarketPlaceGame.java
+++ src/main/java/com/lgs/eden/views/marketplace/MarketPlaceGame.java
@@ -1,24 +1,47 @@
 package com.lgs.eden.views.marketplace;
 
+import com.lgs.eden.api.games.MarketplaceGameData;
 import com.lgs.eden.utils.Utility;
 import com.lgs.eden.utils.ViewsPath;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 
 /**
  * Controller for market_game.fxml
  */
 public class MarketPlaceGame {
 
-    public static Parent getScreen() {
+    public static Parent getScreen(MarketplaceGameData data) {
         FXMLLoader loader = Utility.loadView(ViewsPath.MARKETPLACE_GAME.path);
         Parent parent = Utility.loadViewPane(loader);
         MarketPlaceGame controller = loader.getController();
-        controller.init();
+        controller.init(data);
         return parent;
     }
 
-    private void init() {
+    // ------------------------------ INSTANCE ----------------------------- \\
 
+    @FXML
+    private Button downloadButton;
+    @FXML
+    private Label gameDesc;
+    @FXML
+    private ImageView gameIcon;
+    @FXML
+    private Label gameName;
+    @FXML
+    private ImageView gameImage;
+    @FXML
+    private FlowPane languages;
+    @FXML
+    private FlowPane tags;
+
+    private void init(MarketplaceGameData data) {
+        // ...
     }
 }
