245f08cfcc2480a8f9c5f92058a36b52b057bdb4
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-06-08 20:18:49 +0200

    no conversation view done

--- src/main/java/com/lgs/eden/views/profile/messages/Messages.java
+++ src/main/java/com/lgs/eden/views/profile/messages/Messages.java
@@ -1,9 +1,20 @@
 package com.lgs.eden.views.profile.messages;
 
+import com.lgs.eden.api.API;
+import com.lgs.eden.api.profile.friends.FriendConversationView;
+import com.lgs.eden.application.AppWindowHandler;
+import com.lgs.eden.utils.Translate;
 import com.lgs.eden.utils.Utility;
 import com.lgs.eden.utils.ViewsPath;
+import com.lgs.eden.views.profile.Profile;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.TextFlow;
 
 /**
  * Controller for message.fxml view
@@ -25,7 +36,45 @@ public class Messages {
 
     // ------------------------------ INSTANCE ----------------------------- \\
 
-    private void init(int friendID) {
+    @FXML
+    private Label userName;
+    @FXML
+    private Label userID;
+    @FXML
+    private ListView<?> messages;
+    @FXML
+    private TextArea inputMessage;
+    @FXML
+    private Button sendMessage;
+    @FXML
+    private ListView<?> userList;
+    @FXML
+    private Button profileButton;
+    @FXML
+    private TextFlow friendNameTag;
 
+    // save friendID
+    private int friendID;
+
+    private void init(int friendID) {
+        FriendConversationView conv = API.imp.getMessageWithFriend(friendID);
+        conv = Math.random() > 0?null:conv;
+        if (conv == null){
+            this.friendNameTag.getChildren().clear();
+            this.friendNameTag.getChildren().add(new Label(Translate.getTranslation("no_conv")));
+
+            // disable all
+            this.sendMessage.setDisable(true);
+            this.inputMessage.setDisable(true);
+            this.profileButton.setDisable(true);
+        } else {
+            this.friendID = conv.friendID;
+            System.out.println("blah blah");
+        }
+    }
+
+    @FXML
+    public void goToProfile(){
+        AppWindowHandler.setScreen(Profile.reloadWith(this.friendID), ViewsPath.PROFILE);
     }
 }
