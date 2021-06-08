c0fa9ae36529a14b6f04933689fa7d5fda33e54e
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-06-08 22:24:41 +0200

    conversation cell

--- src/main/java/com/lgs/eden/views/profile/messages/ConversationCell.java
+++ src/main/java/com/lgs/eden/views/profile/messages/ConversationCell.java
@@ -1,8 +1,13 @@
 package com.lgs.eden.views.profile.messages;
 
 import com.lgs.eden.api.profile.friends.conversation.ConversationData;
+import com.lgs.eden.utils.Translate;
 import com.lgs.eden.utils.ViewsPath;
 import com.lgs.eden.utils.cell.CellHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 /**
@@ -18,9 +23,28 @@ public class ConversationCell implements CellHandler<ConversationData> {
 
     // ------------------------------ INSTANCE ----------------------------- \\
 
+    @FXML
+    private Label friendName;
+    @FXML
+    private Label friendStatus;
+    @FXML
+    private ImageView friendAvatar;
+    @FXML
+    private Button newMessages;
+
     @Override
     public void init(ConversationData item) {
+        String message = Translate.getTranslation(item.online ? "online" : "offline");
 
+        this.friendName.setText(item.name);
+        this.friendAvatar.setImage(item.avatar);
+        this.friendStatus.setText(message);
+        // unread messages
+        if (item.unreadMessagesCount > 0){
+            this.newMessages.setText(""+item.unreadMessagesCount);
+        } else {
+            this.newMessages.setVisible(false);
+        }
     }
 
     // ------------------------------ VIEW ----------------------------- \\
