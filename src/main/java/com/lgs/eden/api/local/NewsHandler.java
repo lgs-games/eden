9e764080afcdb216bb59c8f0aee27ef3e29d8b2c
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-06-29 11:32:26 +0200

    local get news

--- src/main/java/com/lgs/eden/api/local/NewsHandler.java
+++ src/main/java/com/lgs/eden/api/local/NewsHandler.java
@@ -2,7 +2,6 @@ package com.lgs.eden.api.local;
 
 import com.lgs.eden.api.news.BasicNewsData;
 import com.lgs.eden.api.news.NewsAPI;
-import com.lgs.eden.utils.config.Language;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -15,7 +14,14 @@ public class NewsHandler implements NewsAPI {
 
     @Override
     public BasicNewsData getNews(String id) {
-        return null;
+        return new BasicNewsData(
+                "Version 3.1.0 released",
+                "/news/news1.png",
+                "We patched a lot of things and tried to improve" +
+                        "the game to make it less easy and more fun to play.",
+                "https://lgs-games.com/api/news/test.md",
+                Date.from(Instant.now())
+        );
     }
 
     @Override
