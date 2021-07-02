8045e2ce18a9a84920a2699af6ff157416a02d7c
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2021-07-02 18:04:17 +0200

    suppress false warnings

--- src/main/java/com/lgs/eden/utils/config/InstallUtils.java
+++ src/main/java/com/lgs/eden/utils/config/InstallUtils.java
@@ -18,7 +18,7 @@ public class InstallUtils {
                 ApplicationCloseHandler.close(false);
 
                 // start exe
-                //noinspection SpellCheckingInspection
+                // lgtm [java/concatenated-command-line]
                 ProcessBuilder process = new ProcessBuilder(installer, "/VERYSILENT", "/MERGETASKS=\"desktopicon,postinstall\"");
                 process.directory(new File(new File(installer).getParent()));
                 process.start();
@@ -43,7 +43,7 @@ public class InstallUtils {
         if (os.equals(OperatingSystem.WINDOWS)) {
             try {
                 // start exe
-                //noinspection SpellCheckingInspection
+                // lgtm [java/concatenated-command-line]
                 ProcessBuilder process = new ProcessBuilder(installer, "/SILENT", "/MERGETASKS=\"desktopicon\"",
                         "/DIR=" + location);
                 process.directory(new File(new File(installer).getParent()));
